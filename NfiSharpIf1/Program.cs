43096100d70c64f3f04b58c4af2daaa3a045bf81
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-10 16:40:12 +0200

    Add project files.

---
+++ NfiSharpIf1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NfiSharpIf1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in ett tal:");
+            int talet = Convert.ToInt32(Console.ReadLine());
+            if (talet > 10)
+            {
+                Console.WriteLine("Talet är större än 10.");
+            }
+            else if(talet < 10)
+            {
+                Console.WriteLine("Talet är mindre än 10.");
+            }
+
+        }
+    }
+}
